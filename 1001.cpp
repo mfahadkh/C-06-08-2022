c472365e423c76ce6e6e766fed07e573995e26f2
mfahadkh <110717445+mfahadkh@users.noreply.github.com>
2022-08-06 15:34:23 +0600

    Add files via upload

---
+++ 1001.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double R;
+    cin>>R>>setprecision(2);
+    double A = 3.14159 * R * R;
+    cout<<"A="<<setprecision(4)<<A<<endl;
+    return 0;
+}
